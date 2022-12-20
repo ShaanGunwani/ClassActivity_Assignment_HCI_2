b50d66d12a40164080cd93483dc0645978edf606
ShaanGunwani <114371881+ShaanGunwani@users.noreply.github.com>
2022-12-20 19:10:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Add your answer here
+$( ".lesson-icon").first().hover(function(){
+    $( ".text-contents").first().show();
+  }, function(){
+    $(".text-contents").hide();
+  })
+$( ".lesson-icon").eq(1).hover(function(){
+    $( ".text-contents").eq(1).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
+$( ".lesson-icon").eq(2).hover(function(){
+    $( ".text-contents").eq(2).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
+$( ".lesson-icon").eq(3).hover(function(){
+    $( ".text-contents").eq(3).show();
+  }, function(){
+    $(".text-contents").hide();
+  })
